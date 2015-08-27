2aa81a4d56e0612abdcb529af9299a9a02bf05d0
Elizabeth Pinchevsky <lizapup@hotmail.com>
2015-08-27 16:22:45 -0700

    Added a background Inserted UI

---
+++ Assets/UItest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UItest : MonoBehaviour {
+
+	int lives = 3;
+	public Text blah;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyUp(KeyCode.A))
+		{
+			lives = lives + 1;
+				print("lives: " + lives);
+			blah.text = "Lives: " + lives;
+
+		}
+	}
+}
---
+++ Assets/scripts/Lives.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameSchoolLibraries;
+
+public class Lives : MonoBehaviour {
+	public GameObject CharacterRobotBoy; 
+	public GameObject Killzone;
+	public int lives; 
+
+	// Use this for initialization
+	void Start () {
+		print ("You have this many lives" + lives);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D thingIHit)
+{
+		if (thingIHit.tag == "Killzone"){ }
+			//god.decreaseLives (1); 
+	}
+}
